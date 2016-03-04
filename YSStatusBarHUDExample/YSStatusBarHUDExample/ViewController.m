3f6dab9e326983ac563e4d041aee96c5df7e7ffb
zfan <zfan1860@gmail.com>
2016-03-04 16:10:51 +0800

    添加一个方法
    添加一个方法

--- YSStatusBarHUDExample/YSStatusBarHUDExample/ViewController.m
+++ YSStatusBarHUDExample/YSStatusBarHUDExample/ViewController.m
@@ -40,6 +40,11 @@
     [YSStatusBarHUD showSuccess:@"加载成功!"];
 }
 
+- (void)show {
+    
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
